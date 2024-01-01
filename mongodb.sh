d0902563cff302b49387c7fa18ee1f5c9be9d6ae
theppasiva <theppasiva@gmail.com>
2024-01-01 17:56:02 +0530

    Roboshop shell script

---
+++ mongodb.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 .... $R FAILED$N"
+    else
+        echo -e "$2 .......$G Success$N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR: please run this script with root access $N"
+    exit 1
+else
+    echo "you are root user"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo 
+
+VALIDATE $? "Copied MONGODB Repo"
