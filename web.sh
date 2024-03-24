6cdf200bc5e324c19d5a64d3a1918644d34de3b4
theppasiva <theppasiva@gmail.com>
2024-03-24 12:26:30 +0530

    Roboshop shel script

--- web.sh
+++ web.sh
@@ -63,6 +63,6 @@ cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf 
 
 VALIDATE $? "copied roboshop reverse proxy config"
 
-systemctl restart nginx &>> $LOGFILE
+systemctl restart nginx 
 
 VALIDATE $? "restarted nginx"
