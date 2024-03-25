1464c2d5a42cc3530c4e2b28e962561d4c44e142
theppasiva <theppasiva@gmail.com>
2024-03-25 18:50:15 +0530

    Roboshop shell script

--- roboshop.sh
+++ roboshop.sh
@@ -15,4 +15,5 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-072ef4397e9b05311 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $INSTANCE_TYPE --security-group-ids sg-072ef4397e9b05311 
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
