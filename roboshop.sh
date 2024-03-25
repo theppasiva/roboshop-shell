e2ceb13d09b4bebc7ba7fc2629eca20af7273fc8
theppasiva <theppasiva@gmail.com>
2024-03-25 17:59:10 +0530

    Roboshop shell script

--- roboshop.sh
+++ roboshop.sh
@@ -17,5 +17,5 @@ do
 
     aws ec2 run-instances --image-id ami-0f3c7d07486cad139  --instance-type $INSTANCE_TYPE 
     --security-group-ids sg-072ef4397e9b05311 --tag-specifications "ResourceType=instance,
-    Tags=[{Key=webserver,Value=$i}]"
+    Tags=[{Key=name,Value=$i}]"
 done
